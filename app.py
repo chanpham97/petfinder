4e2de0b3922b310f407c69761730861b71f93db1
Chan Pham <tqp2001@columbia.edu>
2022-06-15 18:38:31 +0000

    base templates

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
+from flask import render_template
+from flask import request, redirect
+from model import get_breakfast_rating
+from datetime import datetime
 
 # -- Initialization section --
 app = Flask(__name__)
 
 
 # -- Routes section --
+# why do we need this when we can have multiple routes without flask? with just linking html pages
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+  props = {
+    "title" : "breakfast shouter",
+    "name" : "chan"
+  }
+  return render_template("index.html", time=datetime.now(), props=props)
+
+@app.route('/results', methods = ['GET', 'POST'])
+def results():
+    # why request? how does the method get this?
+    if request.method == "POST":
+      print(request.form['breakfast'])
+      breakfast = request.form['breakfast']
+      name = request.form['nickname']
+      rating = get_breakfast_rating(breakfast)
+      return render_template("results.html", breakfast = breakfast, name = name, rating = rating)
+    else:
+      # when running on replit, getting error but works in separate tab
+      return redirect("/")
+  
+@app.route('/secret')
+def secret():
+    return "<h1>secret chan</h1>"
---
+++ model.py
@@ -0,0 +1,6 @@
+# why not just put this in the {{}} if it runs python
+def get_breakfast_rating(food):
+  if food.lower() == "waffles":
+    return "yum"
+  else:
+    return "ok"
